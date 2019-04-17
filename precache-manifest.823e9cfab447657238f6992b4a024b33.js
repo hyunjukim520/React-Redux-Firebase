218d81c5b127ab729f7f7ef20299008861668363
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-04-17 15:34:27 +1000

    Updates

---
+++ precache-manifest.823e9cfab447657238f6992b4a024b33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d142cab24c815dbec8",
+    "url": "/React-Redux-Firebase/static/js/runtime~main.a144d9a4.js"
+  },
+  {
+    "revision": "90e95d6ecd59c01d8d5e",
+    "url": "/React-Redux-Firebase/static/js/main.322d8669.chunk.js"
+  },
+  {
+    "revision": "59f27a1cca96a7c9d10e",
+    "url": "/React-Redux-Firebase/static/js/2.7238c1c4.chunk.js"
+  },
+  {
+    "revision": "90e95d6ecd59c01d8d5e",
+    "url": "/React-Redux-Firebase/static/css/main.4e3ce51c.chunk.css"
+  },
+  {
+    "revision": "75b0dd42ebede2d620cfe1044873ee8d",
+    "url": "/React-Redux-Firebase/index.html"
+  }
+];
